2412cef97f46eb3f0fb6cee22a322d762d68e82d
Matthew Dees <matt@lessthan3.net>
2013-11-12 19:33:07 -0600

    Reworked karma support

--- lib/GeckBot/Plugins/Karma.pm
+++ lib/GeckBot/Plugins/Karma.pm
@@ -15,9 +15,8 @@ sub said
             return;
         }
         $key =~ s/^\W*(\w+)\W*$/$1/g;
-        return if $key eq $said_hr->{'who'};
         my $channel_id = $self->get_channel_id($said_hr->{'channel'});
-        my $value = change_value($self, $operation, $channel_id, $key);
+        my $value = change_value($self, $operation, $channel_id, $key, 1);
         return "Karma for $key is now " . $value;
     }
     return;
@@ -32,30 +31,43 @@ sub get_karma
 {
     my ($self, $said_hr) = @_;
 
-    my $key    = $said_hr->{'body'};
-    my $result = $self->schema->resultset('Karma')->find(
-        {
-            'channel_id' => $self->get_channel_id($said_hr->{'channel'}),
-            'key'        => $key,
-        },
-    );
-    my $value = defined $result ? $result->value : 0;
+    my ( $key, $operation ) = split /\s/, $said_hr->{'body'}, 2;
+
+    my $channel_id = $self->get_channel_id($said_hr->{'channel'});
+    my $value;
+
+    if ( defined $operation && $operation =~ /\s*\+\s*(\d+)/) {
+        $value = change_value($self, '++', $channel_id, $key, $1);
+    }
+    elsif ( defined $operation && $operation =~ /\s*\-\s*(\d+)/) {
+        $value = change_value($self, --, $channel_id, $key, $1);
+    }
+    else {
+        my $result = $self->schema->resultset('Karma')->find(
+            {
+                'channel_id' => $channel_id,
+                'key'        => $key,
+            },
+        );
+        $value = defined $result ? $result->value : 0;
+    }
+
     return "$key has $value karma";
 
 }
 
 sub change_value
 {
-    my ($self, $operation, $channel_id, $key) = @_;
+    my ($self, $operation, $channel_id, $key, $value) = @_;
     my $value;
     my $karma =
       $self->schema->resultset('Channel')->find({ 'id' => $channel_id })
       ->karma->find_or_new({ key => $key });
     if ($operation eq '++') {
-        $karma->value($karma->value + 1);
+        $karma->value($karma->value + $value);
     }
     else {
-        $karma->value($karma->value - 1);
+        $karma->value($karma->value - $value);
     }
     $karma->insert_or_update;
     return $karma->value;
